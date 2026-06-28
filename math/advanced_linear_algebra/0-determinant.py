c109ed120ac7831ba3e61cc45dac54901514057d
kmoutogianni <katerina.mmoutogianni@ai.dlh.lu>
2026-06-28 22:12:38 +0000

    update file 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -6,24 +6,24 @@ def determinant(matrix):
     """calculates the determinant of a matrix"""
 
     # initial checks
-    if type(matrix) is not list:
+    if type(matrix) is not list or matrix == []:
         raise TypeError("matrix must be a list of lists")
-    else:
-        length = len(matrix)
-
-    for i in range(length):
-        row = matrix[i]
-        if row is not list:
-            raise TypeError("matrix must be a list of lists")
-        if len(row) != len(matrix):
-            raise ValueError("matrix must be a square matrix")
-
+    
     # empty matrix
     if matrix == [[]]:
         return 1
 
+    length = len(matrix)
+    for i in range(length):
+        row = matrix[i]
+        if type(row) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(row) != len(matrix):
+            raise ValueError("matrix must be a square matrix")
+
+
     # 1x1 matrix
-    if len(matrix) == 1:
+    if length == 1:
         return matrix[0][0]
 
     # 2x2 matrix
@@ -36,6 +36,6 @@ def determinant(matrix):
         minor = []                              # create the minor
         for row in matrix[1:]:                  # leave out first row
             minor.append(row[:i] + row[i+1:])   # leave out column i
-        det += (-1) ^ i * matrix[0][i] * determinant(minor)  # recursive calc
+        det += (-1) ** i * matrix[0][i] * determinant(minor)  # recursive calc
 
     return det
