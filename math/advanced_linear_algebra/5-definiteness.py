61f73baf379d4800b115d5987676ca04312cd171
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-06-29 20:20:39 +0000

    update file 5

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -4,6 +4,7 @@
 import numpy as np
 
 def definiteness(matrix):
+    """calculates the definiteness of a matrix"""
 
     if type(matrix) is not np.ndarray:
         raise TypeError("matrix must be a numpy.ndarray")
@@ -31,15 +32,3 @@ def definiteness(matrix):
         return "Negative semi-definite"
 
     return "Indefinite"
-
-
-mat1 = np.array([[5, 1], [1, 1]])
-mat2 = np.array([[2, 4], [4, 8]])
-mat3 = np.array([[-1, 1], [1, -1]])
-mat4 = np.array([[-2, 4], [4, -9]])
-mat5 = np.array([[1, 2], [2, 1]])
-mat6 = np.array([])
-mat7 = np.array([[1, 2, 3], [4, 5, 6]])
-mat8 = [[1, 2], [1, 2]]
-
-print(definiteness(mat1))
