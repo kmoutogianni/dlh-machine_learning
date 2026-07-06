83dfb002c42daa5b5488445e405d994ac8a10f74
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-06 15:23:13 +0000

    add file 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""exercise 10"""
+
+
+def poly_derivative(poly):
+    """calculates the derivative of a polynomial"""
+
+    # if poly not valid
+    if type(poly) is not list:
+        return None
+    if len(poly) == 0:
+        return None
+    for element in poly:
+        if type(element) is not int or element < 0:
+            return None
+
+    # if the derivative is 0
+    if len(poly) == 1:
+        deriv_poly = [0]
+        return deriv_poly
+
+    # calculate derivative polynomial
+    deriv_poly = []
+    for i in range(1, len(poly)):
+        d = poly[i] * i
+        deriv_poly.append(d)
+    return deriv_poly
