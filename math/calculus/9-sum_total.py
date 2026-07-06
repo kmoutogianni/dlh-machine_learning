6b8f6a89c46546fbbffd0754c0a55e1a97fa9efc
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-06 14:55:28 +0000

    file 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,7 +5,7 @@
 def summation_i_squared(n):
     """calculates the sum of squares"""
 
-    if type(n) is not int:
+    if type(n) is not int or n < 0:
         return None
 
     sum = n * (n + 1) * (2 * n + 1) / 6
