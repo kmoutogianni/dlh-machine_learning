680277d803c8a4ed1bebb9f767c542ef1158fc07
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-06 21:06:53 +0000

    update file 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -11,6 +11,8 @@ def poly_integral(poly, C=0):
         return None
     if len(poly) == 0:
         return None
+    if poly == [0]:
+        return None
 
     new_poly = []
     new_poly.append(C)
