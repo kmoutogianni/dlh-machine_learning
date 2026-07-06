0293f9621bb32ddd82a18faea04f0de7cbe0033e
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-06 14:49:40 +0000

    file 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""exercise 9"""
+
+
+def summation_i_squared(n):
+    """calculates the sum of squares"""
+
+    if n is not int:
+        return None
+
+    return n * (n + 1) * (2 * n + 1) / 6
