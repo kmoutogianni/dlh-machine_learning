4490b2b24a51e3d6707e6a1eb70c0b60f1a09649
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-11 20:35:49 +0000

    update file 4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -14,9 +14,13 @@ def frequency():
 
     # your code here
     plt.xlim(0, 100)
+#    plt.yticks(np.arange(0, 35, 5))
+#    plt.xticks(np.arange(0, 101, 10))
     bins = np.arange(0, 101, 10)  # bins every 10 units
     plt.title('Project A')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
-    plt.hist(student_grades, bins, edgecolor='black')    # bar outline in black
+    plt.hist(student_grades, bins=bins, edgecolor='black')
     plt.show()
+
+# %%
