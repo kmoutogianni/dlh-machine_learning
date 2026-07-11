eb9a71c6f9843e02a2e3a46a1c661a9c3ddb98ad
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-11 14:47:06 +0000

    update file 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,10 +2,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """plots y as a line graph"""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    
+
     # your code here
+    plt.plot(y, 'r-')
+    plt.show()
+
 line()
