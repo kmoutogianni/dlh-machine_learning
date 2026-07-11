79cf15d95d832307da85f7ae157baacb36bd03b0
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-11 20:08:52 +0000

    update file 4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,6 +13,7 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
 
     # your code here
+    plt.xlim(0, 100)
     bins = np.arange(0, 101, 10)  # bins every 10 units
     plt.title('Project A')
     plt.xlabel('Grades')
