854630edc3bb4c3ef4cab381c13e5131d93f032d
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-12 12:18:41 +0000

    add first draft file 5

---
+++ math/plotting/5-all_in_one.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""plotting exercise 5"""
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+
+# %%
+def all_in_one():
+    """plots all 5 previous graphs in one figure"""
+    y0 = np.arange(0, 11) ** 3
+
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x1, y1 = np.random.multivariate_normal(mean, cov, 2000).T
+    y1 += 180
+
+    x2 = np.arange(0, 28651, 5730)
+    r2 = np.log(0.5)
+    t2 = 5730
+    y2 = np.exp((r2 / t2) * x2)
+
+    x3 = np.arange(0, 21000, 1000)
+    r3 = np.log(0.5)
+    t31 = 5730
+    t32 = 1600
+    y31 = np.exp((r3 / t31) * x3)
+    y32 = np.exp((r3 / t32) * x3)
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+
+    # your code here
+
+    fig = plt.figure(figsize=(6.4, 4.8))
+    fig.suptitle("All in One")
+
+    # first subplot
+    plt.subplot(3, 2, 1)
+
+    plt.xlim(0, 10)
+    plt.plot(y0, 'r-')
+    plt.show()
+
+    # second subplot
+    plt.subplot(3, 2, 2)
+
+    plt.scatter(x1, y1, color='magenta')
+    plt.title('Men\'s Height vs Weight', fontsize='x-small')
+    plt.xlabel('Height (in)', fontsize='x-small')
+    plt.ylabel('Weight (lbs)', fontsize='x-small')
+
+    # third subplot
+    plt.subplot(3, 2, 3)
+
+    plt.title('Exponential Decay of C-14', fontsize='x-small')
+    plt.xlabel('Time (years)', fontsize='x-small')
+    plt.ylabel('Fraction Remaining', fontsize='x-small')
+    plt.xlim(0, 28650)
+    plt.yscale('log')
+    plt.plot(x2, y2)
+
+    # fourth subplot
+    plt.subplot(3, 2, 4)
+
+    plt.title('Exponential Decay of Radioactive Elements', fontsize='x-small')
+    plt.xlabel('Time (years)', fontsize='x-small')
+    plt.ylabel('Fraction Remaining', fontsize='x-small')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+
+    plt.plot(x3, y31, 'r--', label='C-14', fontsize='x-small')
+    plt.plot(x3, y32, 'g-', label='Ra-226', fontsize='x-small')
+
+    plt.legend(loc='upper right', fontsize='x-small')
+
+    # fifth subplot
+    plt.subplot2grid((3, 2), (2, 0), colspan=2)
+
+    plt.title('Project A', fontsize='x-small')
+    plt.xlabel('Grades', fontsize='x-small')
+    plt.ylabel('Number of Students', fontsize='x-small')
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+
+    plt.show()
+
+all_in_one()
