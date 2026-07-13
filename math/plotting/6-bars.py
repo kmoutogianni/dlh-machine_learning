37836b032e2d6d59c75f731cfa3d7a686c1d35c9
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-13 21:32:12 +0000

    update file 6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 
 def bars():
+    """plots a stacked bar graph"""
+
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
