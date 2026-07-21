5c15e2f8988913537f800c794b63907804eb1a68
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-21 13:17:49 +0000

    add file for exponential distribution

---
+++ math/probability/exponential.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""task 3-5"""
+
+
+class Exponential:
+    """class representing an exponential distribution"""
+
+
+def __init__(self, data=None, lambtha=1.):
+    """sets the instance attribute lambtha"""
+    if data is None:
+        if lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        self.lambtha = float(lambtha)
+
+    else:
+        if type(data) is not list:
+            raise TypeError("data must be a list")
+        if len(data) < 2:
+            raise ValueError("data must contain multiple values")
+        self.lambtha = float(sum(data) / len(data))
