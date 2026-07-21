eda257e2b3cb5851aed6eaf38f0084e9436dbf1c
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-21 16:40:52 +0000

    add file for binomial  distribution

---
+++ math/probability/binomial.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""tasks 10-12"""
+
+
+class Binomial:
+    """class representing a binomial distribution"""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """Sets the instance attributes n and p"""
+
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+
+            if p < 0 or p > 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+
+            self.n = int(n)
+            self.p = float(p)
+
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            mean = sum(data) / len(data)
+
+            var = 0
+            for x in data:
+                var += (x - self.mean)**2
+            var = var / len(data)
+
+            p = 1 - variance / mean
+            n = mean / p
+            n = round(n)
+
+            self.n = int(n)
+            self.p = float(mean / self.n)
