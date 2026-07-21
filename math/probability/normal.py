d2a705de4271e1cb00033e455d71b26e8875bbd0
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-21 15:23:21 +0000

    update file for normal distribution

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -41,3 +41,12 @@ class Normal:
 
         x = self.mean + z * self.stddev
         return x
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        exp = -(x - self.mean)**2 / (2 * (self.stddev ** 2))
+        f = 1 / (self.stddev * ((2*pi)**0.5)) * e ** exp
+        return f
