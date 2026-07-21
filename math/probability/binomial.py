aff7daf4352b12a42e871786bdfb1303c4af7de5
kmoutogianni <katerina.moutogianni@ai.dlh.lu>
2026-07-21 17:09:37 +0000

    update file for binomial  distribution

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -12,7 +12,7 @@ class Binomial:
             if n <= 0:
                 raise ValueError("n must be a positive value")
 
-            if p < 0 or p > 1:
+            if p <= 0 or p > 1:
                 raise ValueError("p must be greater than 0 and less than 1")
 
             self.n = int(n)
